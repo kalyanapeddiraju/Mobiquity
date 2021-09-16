513d1593a48435457793c528aa0e3667e989ef0e
Sailaja Machiraju <sailajamachiraju@Sailajas-MBP.home>
2021-09-16 18:47:45 +0100

    Fix mavan build run issues

--- src/test/java/com/org/testapi/jsonplaceholderAPItest.java
+++ src/test/java/com/org/testapi/jsonplaceholderAPItest.java
@@ -1,13 +1,10 @@
 package com.org.testapi;
 
-
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import com.org.libapiresthelper.RestResponse;
 import io.qameta.allure.Description;
-import io.qameta.allure.Step;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.skyscreamer.jsonassert.JSONCompareMode;
@@ -17,7 +14,7 @@ import com.org.libapiresthelper.Helpers;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class testAPI {
+public class jsonplaceholderAPItest {
 
     com.org.libapiresthelper.RequestBuilder requestBuilder;
     com.org.libapiresthelper.RestResponse restResponse;
@@ -36,7 +33,7 @@ public class testAPI {
         actualjsonResponseGetUser = new JSONObject(actualResponse);
         //String actualResponse = restResponse.getResponse().path("find{it.username=='Delphine'}").toString();
         Helpers.jsonXAssertEquals("Validate Get User Response", actualjsonResponseGetUser.toString(), exceptedResponseGetUsers, JSONCompareMode.STRICT);
-        // Get the Id for the specific user
+        // Get the userId for the specific user
         String id = restResponse.getResponse().path("find{it.username=='Delphine'}.id").toString();
         System.out.println(" Get User Response  >>   " + actualjsonResponseGetUser.toString());
         System.out.println(" Get User Response  Actual HTTP Status Code  >>   " + restResponse.getStatusCode());
@@ -67,7 +64,7 @@ public class testAPI {
     }
     @Test (description = " To get Multiple posts")
     @Description (" Description : Testing the  Posts Endpoint whether return multiple results")
-    public void getMultiplePosts() throws JSONException, IOException {
+    public void getMultiplePosts() {
         //Get multiple Posts
         GETPostsEndpoint = "https://jsonplaceholder.typicode.com/posts/1/comments";
         requestBuilder = new RequestBuilder("GET", GETPostsEndpoint, "");
@@ -79,7 +76,7 @@ public class testAPI {
     }
     @Test (description = " To Check Exception Cases")
     @Description (" Description : Testing the GET Post Endpoint whether return proper exception HTTP Status")
-    public void getMultiplePostsException_TC_1() throws JSONException, IOException {
+    public void getMultiplePostsException_TC_1() {
         //Get multiple Posts
         GETPostsEndpoint = "https://jsonplaceholder.typicode.com/posts/1456/comments";
         requestBuilder = new RequestBuilder("GET", GETPostsEndpoint, "");
@@ -93,7 +90,7 @@ public class testAPI {
     }
     @Test (description = " To Check Exception Cases")
     @Description (" Description : Testing the GET Post Endpoint whether return proper exception HTTP Status")
-    public void getMultiplePostsExceptionTC_2() throws JSONException, IOException {
+    public void getMultiplePostsExceptionTC_2() {
         //Get multiple Posts
         GETPostsEndpoint = "https://jsonplaceholder.typicode.com/posts/1456";
         requestBuilder = new RequestBuilder("GET", GETPostsEndpoint, "");
@@ -105,7 +102,7 @@ public class testAPI {
 
     @Test (description = " To Check the Comments Endpoint ")
     @Description (" Description : Testing the Comments Endpoint whether return multiple results")
-    public void getMultipleCommentsTC_1() throws JSONException, IOException {
+    public void getMultipleCommentsTC_1() {
         //Get multiple Posts
         GETCommentsEndpoint = "https://jsonplaceholder.typicode.com/comments/";
         requestBuilder = new RequestBuilder("GET", GETCommentsEndpoint, "");
@@ -118,7 +115,7 @@ public class testAPI {
 
     @Test (description = " To Check the Comments Endpoint by passing query parameter ")
     @Description (" Description : Testing the Comments Endpoint whether return multiple results")
-    public void getMultipleCommentsTC_2() throws JSONException, IOException {
+    public void getMultipleCommentsTC_2() {
         //Get multiple Posts
         GETCommentsEndpoint = "https://jsonplaceholder.typicode.com/comments?PostId=1";
         requestBuilder = new RequestBuilder("GET", GETCommentsEndpoint, "");
@@ -131,7 +128,7 @@ public class testAPI {
 
     @Test (description = " To Check the Comments Endpoint by passing wrong query parameter values ")
     @Description (" Description : Testing the /Comments Endpoint whether return proper exception https status codes")
-    public void getMultipleCommentsTC_3() throws JSONException, IOException {
+    public void getMultipleCommentsTC_3() {
         //Get multiple Posts
         GETCommentsEndpoint = "https://jsonplaceholder.typicode.com/comments?PostId=12345";
         requestBuilder = new RequestBuilder("GET", GETCommentsEndpoint, "");
@@ -143,7 +140,7 @@ public class testAPI {
 
     @Test (description = " To Check the Comments Endpoint by passing wrong Id ")
     @Description (" Description : Testing the /Comments Endpoint whether return proper exception https status codes")
-    public void getMultipleCommentsTC_4() throws JSONException, IOException {
+    public void getMultipleCommentsTC_4() {
         //Get multiple Posts
         GETCommentsEndpoint = "https://jsonplaceholder.typicode.com/comments/12345";
         requestBuilder = new RequestBuilder("GET", GETCommentsEndpoint, "");
